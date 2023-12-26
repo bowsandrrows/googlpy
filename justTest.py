654fed12ae0006549ce4d06e2157041202092f0e
bowsandrrows <154934324+bowsandrrows@users.noreply.github.com>
2023-12-26 23:58:22 +0100

    Update justTest.py

--- justTest.py
+++ justTest.py
@@ -21,25 +21,9 @@ def verbing(s):
     else:
         return s
 
-
 print(verbing('swimming'))
 
-"""A unicode string is a different type of object from a byte string but various libraries such as regular 
-expressions work correctly if passed either type of string.
-
-To convert a regular Python string to bytes, call the encode() method on the string. Going the other direction, 
-the byte string decode() method converts encoded plain bytes to a unicode string:"""
-
-ustring = 'A unicode \u018e string \xf1'
-z = ustring.encode('utf-8')
-print(z)
-# b'A unicode \xc6\x8e string \xc3\xb1'
-
-# bytes of utf-8 encoding. Note the b-prefix.
-t = z.decode('utf-8')  # Convert bytes back to a unicode string
-var = t == ustring  # It's the same as the original, yay!
-print(var)  # True
-
+#=================================================================================
 
 def not_bad(s):
     char_list = ['!', '.', '?']
